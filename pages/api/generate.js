6d80dd66649c52c4ec90289d0edc64aa9e547379
Okera Johnson <okerajohnson@gmail.com>
2023-06-11 11:04:08 -0400

    pro plan duration

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,5 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
+
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -123,8 +125,8 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(name, prodserv, mission, problem, descrip, hero, colors, villian, vision, future, solution ),
       temperature: 1,
-      
       max_tokens: 1042,
+      stream: true,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
@@ -139,6 +141,8 @@ export default async function (req, res) {
           message: 'An error occurred during your request.',
         }
       });
+       const stream = await OpenAIStream(payload);
+        return new Response(stream);
     }
   }
 }
